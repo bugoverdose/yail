eb20670f77be501535fdabfd4a19fb35a8f09da3
Jeong Jinwoo <jwjeong96@gmail.com>
2022-12-31 16:40:20 +0900

    feat(object): implement Error object for evaluation errors

---
+++ src/yail/object/error.go
@@ -0,0 +1,21 @@
+package object
+
+import "fmt"
+
+const ERROR_OBJ = "ERROR"
+
+type Error struct {
+	Message string
+}
+
+func NewError(format string, a ...interface{}) *Error {
+	return &Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func (e *Error) Type() ObjectType {
+	return ERROR_OBJ
+}
+
+func (e *Error) Inspect() string {
+	return "ERROR: " + e.Message
+}

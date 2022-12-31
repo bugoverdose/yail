ec5f90919579af29048aa9896f3ed18e01a9a9a6
Jeong Jinwoo <jwjeong96@gmail.com>
2022-12-31 17:02:35 +0900

    feat(evaluator): implement variable binding and add Environment as parameter of Eval function

--- src/yail/evaluator/evaluator.go
+++ src/yail/evaluator/evaluator.go
@@ -5,30 +5,55 @@ import (
 	"yail/ast/expression"
 	"yail/ast/node"
 	"yail/ast/statement"
+	"yail/environment"
 	"yail/object"
+	"yail/token"
 )
 
-func Eval(node node.Node) object.Object {
+func Eval(node node.Node, env *environment.Environment) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalProgram(node)
+		return evalProgram(node, env)
 
 	// TODO: Statement 노드인지 체크하고 별도로 분기
 	case *statement.ExpressionStatement:
-		return Eval(node.Expression)
+		return Eval(node.Expression, env)
+	case *statement.VariableBinding:
+		val := Eval(node.Value, env)
+		// TODO: 할당하려는 우항 평가에서 문제 있는 경우에 대한 예외 처리 추가
+		var ok bool
+		if node.Token.Type == token.VAL {
+			ok = env.ImmutableAssign(node.Name.Value, val)
+		}
+		if node.Token.Type == token.VAR {
+			ok = env.MutableAssign(node.Name.Value, val)
+		}
+		if !ok {
+			return nil // TODO: 예외처리 로직 제대로 추가
+		}
 
 	// TODO: Expression 노드인지 체크하고 별도로 분기
+	case *expression.Identifier:
+		return evalIdentifier(node, env)
 	case *expression.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 	return nil
 }
 
-func evalProgram(program *ast.Program) object.Object {
+func evalProgram(program *ast.Program, env *environment.Environment) object.Object {
 	var result object.Object
 
 	for _, statement := range program.Statements {
-		result = Eval(statement)
+		result = Eval(statement, env)
 	}
 	return result
+}
+
+func evalIdentifier(node *expression.Identifier, env *environment.Environment) object.Object {
+	val, ok := env.Get(node.Value)
+	if !ok {
+		return object.NewError("identifier not found: " + node.Value)
+	}
+	return val
 }
--- src/yail/evaluator/evaluator_test.go
+++ src/yail/evaluator/evaluator_test.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
 	"testing"
+	"yail/environment"
 	"yail/lexer"
 	"yail/object"
 	"yail/parser"
@@ -22,10 +23,49 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestVariableBindingStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"var a = 5; a;", 5},
+		{"var a = 5; val b = a; b;", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestErrorHandling(t *testing.T) {
+	tests := []struct {
+		input           string
+		expectedMessage string
+	}{
+		{
+			"x",
+			"identifier not found: x",
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		errObj, ok := evaluated.(*object.Error)
+		if !ok {
+			t.Errorf("no error object returned. got=%T(%+v)", evaluated, evaluated)
+			continue
+		}
+		if errObj.Message != tt.expectedMessage {
+			t.Errorf("wrong error message. expected=%q, got=%q", tt.expectedMessage, errObj.Message)
+		}
+	}
+}
+
 func testEval(input string) object.Object {
 	p := parser.New(lexer.New(input))
 	program := p.ParseProgram()
-	return Eval(program)
+	env := environment.NewEnvironment()
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) {
